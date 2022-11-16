db2f00138918992de390f98137fb454314a3c0c5
jklaiven <87581758+jklaiven@users.noreply.github.com>
2022-11-15 21:30:16 -0300

    Add files via upload

---
+++ script/interface.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded',()=>{
+
+
+    let squares = document.querySelectorAll(".square");
+
+    squares.forEach((square)=>{
+        square.addEventListener('click', handleClick);
+  })
+
+})
+
+function handleClick(event) {
+
+  let square = event.target;
+  let postion = square.id;
+
+  if (handleMove(postion)) {
+
+      setTimeout(() => {
+        alert("O jogo Acabou - O Vencedor foi " + playerTime);
+
+      }, 10);
+
+  };
+  updateSquares(postion);
+
+}
+
+function updateSquares(postion) {
+
+  let square = document.getElementById(postion.toString());
+  let symbol = board[postion];
+  square.innerHTML = `<div class='${symbol}'></div>`
+}
